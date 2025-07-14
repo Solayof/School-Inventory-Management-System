8c9060d3bdfa98ea13c94405fb984049e409aba2
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-14 16:53:46 +0100

    set up spring boot app

---
+++ src/main/java/com/solayof/schoolinventorymanagement/SchoolinventorymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.solayof.schoolinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolinventorymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolinventorymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solayof/schoolinventorymanagement/SchoolinventorymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solayof.schoolinventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolinventorymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
