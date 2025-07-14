68ee7138754795abfe2b531de8dd35868f2bbe68
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-14 18:27:09 +0100

    create item status enum

---
+++ src/main/java/com/solayof/schoolinventorymanagement/constants/Status.java
@@ -0,0 +1,10 @@
+package com.solayof.schoolinventorymanagement.constants;
+
+/**
+ * Enum representing the possible statuses for an inventory item.
+ */
+public enum Status {
+    AVAILABLE,
+    ASSIGNED,
+    RETURNED
+}
