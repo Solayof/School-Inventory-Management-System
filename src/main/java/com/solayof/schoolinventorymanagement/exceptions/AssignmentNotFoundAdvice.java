a37a1d30bc427de5cc09f6211f0e60d72df6544c
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-15 04:47:14 +0100

    update assignment not found advice response

--- src/main/java/com/solayof/schoolinventorymanagement/exceptions/AssignmentNotFoundAdvice.java
+++ src/main/java/com/solayof/schoolinventorymanagement/exceptions/AssignmentNotFoundAdvice.java
@@ -2,11 +2,14 @@ package com.solayof.schoolinventorymanagement.exceptions;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestControllerAdvice
 public class AssignmentNotFoundAdvice {
+
     @ExceptionHandler(AssignmentNotFoundException.class)
-    public String handleAssignmentNotFound(AssignmentNotFoundException ex) {
-        return ex.getMessage();
+    public ResponseEntity<String> handleAssignmentNotFound(AssignmentNotFoundException ex) {
+        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
