34b322eb6f4ca1d989444709b3bebb19e8d421fb
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-15 13:19:54 +0100

    create reminder model assembler

---
+++ src/main/java/com/solayof/schoolinventorymanagement/modelAssembler/ReminderModelAssembler.java
@@ -0,0 +1,26 @@
+package com.solayof.schoolinventorymanagement.modelAssembler;
+
+
+import com.solayof.schoolinventorymanagement.dtos.ReminderDTO;
+import com.solayof.schoolinventorymanagement.entity.Reminder;
+import com.solayof.schoolinventorymanagement.restControllers.ReminderController;
+
+import org.springframework.lang.NonNull;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@Component
+public class ReminderModelAssembler implements RepresentationModelAssembler<Reminder, EntityModel<ReminderDTO>> {
+    @SuppressWarnings("null")
+    @Override
+    public EntityModel<ReminderDTO> toModel(@NonNull Reminder reminder) {
+        return EntityModel.of(
+            ReminderDTO.fromReminder(reminder),
+            linkTo(methodOn(ReminderController.class).getOne(reminder.getId())).withSelfRel(),
+            linkTo(methodOn(ReminderController.class).createItem(ReminderDTO.fromReminder(reminder))).withRel("createItem")
+        );
+    }
+}
