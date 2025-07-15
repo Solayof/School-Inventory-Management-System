3e455d67d1744e9f17b8b58ed5a7f0609a903f6b
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-15 04:49:33 +0100

    update reminder not found advice response

--- src/main/java/com/solayof/schoolinventorymanagement/exceptions/ReminderNotFoundAdvice.java
+++ src/main/java/com/solayof/schoolinventorymanagement/exceptions/ReminderNotFoundAdvice.java
@@ -2,11 +2,14 @@ package com.solayof.schoolinventorymanagement.exceptions;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 
 @RestControllerAdvice
 public class ReminderNotFoundAdvice {
     @ExceptionHandler(ReminderNotFoundException.class)
-    public String handleReminderNotFound(ReminderNotFoundException ex) {
-        return ex.getMessage();
+    public ResponseEntity<String> handleReminderNotFound(ReminderNotFoundException ex) {
+        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
