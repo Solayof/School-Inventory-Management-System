b29c832f94e8c2161a5e3fb4daa3bd76f4e4be06
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-15 04:52:15 +0100

    update category service

--- src/main/java/com/solayof/schoolinventorymanagement/services/CategoryService.java
+++ src/main/java/com/solayof/schoolinventorymanagement/services/CategoryService.java
@@ -1,11 +1,12 @@
 package com.solayof.schoolinventorymanagement.services;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.solayof.schoolinventorymanagement.constants.CategoryName;
 import com.solayof.schoolinventorymanagement.entity.Category;
 import com.solayof.schoolinventorymanagement.exceptions.CategoryNotFoundException;
 import com.solayof.schoolinventorymanagement.repository.CategoryRepository;
@@ -30,6 +31,16 @@ public class CategoryService {
         return categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new CategoryNotFoundException("Category not found with id: " + categoryId));
     }
+
+    /**
+     * Checks if a category exists by its ID.
+     * 
+     * @param categoryId the ID of the category to check
+     * @return true if a category with the given ID exists, false otherwise
+     */
+    public boolean existsById(UUID categoryId) {
+        return categoryRepository.existsById(categoryId);
+    }
     
     /**
      * Finds a category by its name.
@@ -38,7 +49,7 @@ public class CategoryService {
      * @return the found Category entity
      * @throws CategoryNotFoundException if no category is found with the given name
      */
-    public Category findByName(CategoryName name) {
+    public Category findByName(String name) {
         return categoryRepository.findByName(name)
                 .orElseThrow(() -> new CategoryNotFoundException("Category not found with name: " + name));
     }
@@ -49,6 +60,30 @@ public class CategoryService {
      * @return the saved Category entity
      */
     public Category saveCategory(Category category) {
+        Optional<Category> existingCategory = categoryRepository.findByName(category.getName());
+        if (existingCategory.isPresent()) {
+            throw new IllegalArgumentException("Category with name '" + category.getName() + "' already exists.");
+        }
         return categoryRepository.save(category);
     }
+
+    /**
+     * findallCategories
+     * Retrieves all categories from the repository.
+     */
+    public List<Category> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    /**
+     * Deletes a category by its ID.
+     *
+     * @param categoryId the ID of the category to delete
+     * @throws CategoryNotFoundException if no category is found with the given ID
+     */
+    public void deleteCategory(UUID categoryId) {
+        Category category = findByCategoryId(categoryId);
+        categoryRepository.delete(category);
+    }
+
 }
