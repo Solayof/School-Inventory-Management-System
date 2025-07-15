24bd318a4012b8ead47f03f12dd183d4e214e53b
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-15 04:45:30 +0100

    create error dto

---
+++ src/main/java/com/solayof/schoolinventorymanagement/dtos/ErrorDTO.java
@@ -0,0 +1,13 @@
+package com.solayof.schoolinventorymanagement.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDTO {
+    private String code;
+    private String message;
+}
