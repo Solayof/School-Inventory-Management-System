7adde66552dd67e5b8cf9267e5724f2f8aa7f829
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-15 04:48:20 +0100

    update item not found advice response

--- src/main/java/com/solayof/schoolinventorymanagement/exceptions/ItemNotFoundAdvice.java
+++ src/main/java/com/solayof/schoolinventorymanagement/exceptions/ItemNotFoundAdvice.java
@@ -2,11 +2,13 @@ package com.solayof.schoolinventorymanagement.exceptions;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestControllerAdvice
 public class ItemNotFoundAdvice {
     @ExceptionHandler(ItemNotFoundException.class)
-    public String handleItemNotFound(ItemNotFoundException ex) {
-        return ex.getMessage();
+    public ResponseEntity<String> handleItemNotFound(ItemNotFoundException ex) {
+        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
