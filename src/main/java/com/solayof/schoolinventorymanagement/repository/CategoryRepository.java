50bc8e0d7a5963d8841b941b6d48f06fe8d0a679
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-15 04:50:47 +0100

    update category repository

--- src/main/java/com/solayof/schoolinventorymanagement/repository/CategoryRepository.java
+++ src/main/java/com/solayof/schoolinventorymanagement/repository/CategoryRepository.java
@@ -1,13 +1,15 @@
 package com.solayof.schoolinventorymanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 import java.util.UUID;
 
-import com.solayof.schoolinventorymanagement.constants.CategoryName;
 import com.solayof.schoolinventorymanagement.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID>{
-   Optional<Category> findByName(CategoryName name); 
+   Optional<Category> findByName(String name);
+   Boolean existsByName(String name);
+   boolean existsById(@NonNull UUID id); 
 }
