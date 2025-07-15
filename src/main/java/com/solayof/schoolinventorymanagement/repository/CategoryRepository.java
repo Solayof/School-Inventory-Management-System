aa5ded6cdb1c895e3050870299277038ecfc3011
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-15 13:20:59 +0100

    add findByNameContainingIgnoreCase method to category repository

--- src/main/java/com/solayof/schoolinventorymanagement/repository/CategoryRepository.java
+++ src/main/java/com/solayof/schoolinventorymanagement/repository/CategoryRepository.java
@@ -3,6 +3,7 @@ package com.solayof.schoolinventorymanagement.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +12,6 @@ import com.solayof.schoolinventorymanagement.entity.Category;
 public interface CategoryRepository extends JpaRepository<Category, UUID>{
    Optional<Category> findByName(String name);
    Boolean existsByName(String name);
+   List<Category> findByNameContainingIgnoreCase(String name);
    boolean existsById(@NonNull UUID id); 
 }
