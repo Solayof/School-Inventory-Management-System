fc7efefe2668e24eb8be05c1b13e4a3a5154d536
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-15 04:47:57 +0100

    update collector not found advice response

--- src/main/java/com/solayof/schoolinventorymanagement/exceptions/CollectorNotFoundAdvice.java
+++ src/main/java/com/solayof/schoolinventorymanagement/exceptions/CollectorNotFoundAdvice.java
@@ -3,10 +3,13 @@ package com.solayof.schoolinventorymanagement.exceptions;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 @RestControllerAdvice
 public class CollectorNotFoundAdvice {
     @ExceptionHandler(CollectorNotFoundException.class)
-    public String handleCollectorNotFound(CollectorNotFoundException ex) {
-        return ex.getMessage();
+    public ResponseEntity<String> handleCollectorNotFound(CollectorNotFoundException ex) {
+        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
