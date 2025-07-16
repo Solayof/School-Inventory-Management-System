c52c43d4df8826f30385d43e9eabe0d745aac818
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-16 01:56:40 +0100

    create user service

---
+++ src/main/java/com/solayof/schoolinventorymanagement/services/UserService.java
@@ -0,0 +1,41 @@
+package com.solayof.schoolinventorymanagement.services;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.solayof.schoolinventorymanagement.entity.UserEntity;
+import com.solayof.schoolinventorymanagement.exceptions.UserNotFoundException;
+import com.solayof.schoolinventorymanagement.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException(email));
+        return UserDetailsImpl.build(user);
+    }
+
+    public UserEntity getUserById(UUID id) throws UserNotFoundException {
+        return userRepository.findById(id)
+        .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    public String addUser(UserEntity user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "UserEntity added Sucessfully";
+    }
+}
