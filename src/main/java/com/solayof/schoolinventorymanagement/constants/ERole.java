6ad585c43208d93ed6ef1f2983573048ce0b6526
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-16 02:22:56 +0100

    add role enum

---
+++ src/main/java/com/solayof/schoolinventorymanagement/constants/ERole.java
@@ -0,0 +1,14 @@
+package com.solayof.schoolinventorymanagement.constants;
+
+public enum ERole {
+    ROLE_SUPERADMIN,
+    ROLE_ADMIN,
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_TUTOR,
+    WRITE,
+    READ,
+    DELETE,
+    UPDATE,
+    ROLE_HOD 
+}
