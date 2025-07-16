2f2330012fcc1ed99bc4c6c09fe848c2366f04ea
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-16 01:54:31 +0100

    create jwtauthfilter

---
+++ src/main/java/com/solayof/schoolinventorymanagement/filter/JwtAuthFilter.java
@@ -0,0 +1,71 @@
+package com.solayof.schoolinventorymanagement.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.solayof.schoolinventorymanagement.services.JwtService;
+import com.solayof.schoolinventorymanagement.services.UserService;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserService userDetailsService;
+    
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException, IOException {
+        try {
+                String authHeader = request.getHeader("Authorization");
+                String cookie = request.getHeader("Cookie");
+                log.info(cookie);
+                String token = null;
+                String username = null;
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    token = authHeader.substring(7);
+                    username = jwtService.extractUsername(token);
+                }
+
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    if (jwtService.validateToken(token, userDetails)) {
+                        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    }
+                }
+                log.info("JWT Authentication successful for user: {}", username);
+
+                    
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.getWriter()
+            .write(new ObjectMapper()
+            .writeValueAsString(Map.of("error", "Unauthorized",
+            "message", "Access denied. You do not have permission to access this resource.",
+            "errMsg", e.getMessage())));
+        }
+    }
+}
