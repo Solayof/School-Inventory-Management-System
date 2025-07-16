6557812c0e61d12141c43848584245e0808f68ec
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-16 02:20:40 +0100

    add springdoc config

---
+++ src/main/java/com/solayof/schoolinventorymanagement/config/SpringDocConfig.java
@@ -0,0 +1,27 @@
+package com.solayof.schoolinventorymanagement.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "Flexisaf Internship", version = "1.0", description = "Backend API to learn Flexisaf internship program"),
+    security = @SecurityRequirement(name = "bearerAuth") 
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    description = "JWT authentication",
+    scheme = "bearer",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    in = SecuritySchemeIn.HEADER
+)
+public class SpringDocConfig {
+    
+}
