ece6f85b0557b858dddf7d318c5e5280e10edce7
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-16 02:27:49 +0100

    add login dto

---
+++ src/main/java/com/solayof/schoolinventorymanagement/dtos/LoginDTO.java
@@ -0,0 +1,22 @@
+package com.solayof.schoolinventorymanagement.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LoginDTO {
+    
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
