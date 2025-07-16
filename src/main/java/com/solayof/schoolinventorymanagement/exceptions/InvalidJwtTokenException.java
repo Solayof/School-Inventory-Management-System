59bcf75a3a1ac8883ae4888b5492d19fd1cdb3cc
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-16 01:52:31 +0100

    create invalid jwt token handler

---
+++ src/main/java/com/solayof/schoolinventorymanagement/exceptions/InvalidJwtTokenException.java
@@ -0,0 +1,7 @@
+package com.solayof.schoolinventorymanagement.exceptions;
+
+public class InvalidJwtTokenException extends RuntimeException {
+    public InvalidJwtTokenException(String message) {
+        super(message);
+    }
+}
