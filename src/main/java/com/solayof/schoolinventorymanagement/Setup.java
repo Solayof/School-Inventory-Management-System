78d73e5a8072a9552ac43e23332ebb0ce487a8d9
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-17 20:47:13 +0100

    update setup file

--- src/main/java/com/solayof/schoolinventorymanagement/Setup.java
+++ src/main/java/com/solayof/schoolinventorymanagement/Setup.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
-import com.github.javafaker.Faker;
 import com.solayof.schoolinventorymanagement.constants.ERole;
 import com.solayof.schoolinventorymanagement.entity.RoleEntity;
 import com.solayof.schoolinventorymanagement.entity.UserEntity;
@@ -14,7 +13,6 @@ import com.solayof.schoolinventorymanagement.repository.RoleRepository;
 import com.solayof.schoolinventorymanagement.repository.UserRepository;
 import com.solayof.schoolinventorymanagement.services.UserService;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -46,50 +44,17 @@ public class Setup implements CommandLineRunner {
             roleRepository.save(role);
         }
 
-        Optional<RoleEntity> optRolehod = roleRepository.findByName(ERole.ROLE_HOD);
-        if(optRolehod.isEmpty()){
+        Optional<RoleEntity> optRoleManger = roleRepository.findByName(ERole.ROLE_MANAGER);
+        if(optRoleManger.isEmpty()){
             RoleEntity role = new RoleEntity();
-            role.setName(ERole.ROLE_HOD);
+            role.setName(ERole.ROLE_MANAGER);
             roleRepository.save(role);
         }
 
-        Optional<RoleEntity> optRoleOwn = roleRepository.findByName(ERole.ROLE_MODERATOR);
-        if(optRoleOwn.isEmpty()){
-            RoleEntity role = new RoleEntity();
-            role.setName(ERole.ROLE_MODERATOR);
-            roleRepository.save(role);
-        }
+        
 
-        Optional<RoleEntity> optRoleMe = roleRepository.findByName(ERole.ROLE_USER);
-        if(optRoleMe.isEmpty()){
-            RoleEntity role = new RoleEntity();
-            role.setName(ERole.ROLE_USER);
-            roleRepository.save(role);
-        }
-
-        Optional<RoleEntity> optRoleTutor = roleRepository.findByName(ERole.ROLE_TUTOR);
-        if(optRoleTutor.isEmpty()){
-            RoleEntity role = new RoleEntity();
-            role.setName(ERole.ROLE_TUTOR);
-            roleRepository.save(role);
-        }
-
-        Optional<RoleEntity> optRoleRead = roleRepository.findByName(ERole.READ);
-        if(optRoleRead.isEmpty()){
-            RoleEntity role = new RoleEntity();
-            role.setName(ERole.READ);
-            roleRepository.save(role);
-        }
-
-        Optional<RoleEntity> optRoleWrite = roleRepository.findByName(ERole.WRITE);
-        if(optRoleWrite.isEmpty()){
-            RoleEntity role = new RoleEntity();
-            role.setName(ERole.WRITE);
-            roleRepository.save(role);
-        }
-
-
-
+        Optional<UserEntity> optUser = userRepository.findByEmail("solayof@gmail.com");
+        if (optUser.isEmpty()) {
         UserEntity superadmin = new UserEntity();
         superadmin.setDob(LocalDate.now());
         superadmin.setEmail("solayof@gmail.com");
@@ -104,29 +69,6 @@ public class Setup implements CommandLineRunner {
         superadmin.setRoles(Set.of(optRoleSupAdmin.get(), optRoleAdm.get()));
 
         userService.addUser(superadmin);
-
-        Faker faker = new Faker();
-
-        List<String> genders = List.of("Male", "Female");
-
-        // for (int i = 0; i < 10; i++) {
-        //     UserEntity sign = new UserEntity();
-        //     sign.setCreatedAt(LocalDateTime.now());
-        //     sign.setDob(LocalDate.now());
-        //     sign.setEmail(faker.internet().emailAddress());
-        //     sign.setFirstName(faker.name().firstName());
-        //     sign.setMiddleName(faker.name().lastName());
-        //     sign.setLastName(faker.name().lastName());
-        //     sign.setGender(genders.get(new Random().nextInt(genders.size())));
-        //     sign.setPassword(faker.lorem().word());
-
-        //     Optional<UserEntity> user = userRepository.findByEmail(sign.getEmail());
-
-        //     if (user.isEmpty()) {
-        //         userService.addUser(sign);
-        //     }
-            
-        // }
-        
+        }
     }
 }
