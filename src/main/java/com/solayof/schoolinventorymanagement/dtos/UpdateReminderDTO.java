011ce9a55fbe27b91a6ef2238f9af00390c247ce
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-07-17 20:51:40 +0100

    edit updatereminder dto

--- src/main/java/com/solayof/schoolinventorymanagement/dtos/UpdateReminderDTO.java
+++ src/main/java/com/solayof/schoolinventorymanagement/dtos/UpdateReminderDTO.java
@@ -2,29 +2,31 @@ package com.solayof.schoolinventorymanagement.dtos;
 
 import java.time.LocalDate;
 
+import com.solayof.schoolinventorymanagement.constants.ReminderStatus;
+
 import jakarta.validation.constraints.Size;
 
 public class UpdateReminderDTO {
    @Size(max = 10, message = "Reminder date must be in the format YYYY-MM-DD")
    private LocalDate reminderDate;
     @Size(max = 10, message = "Status must be at most 10 characters")
-   private String status;
+   private ReminderStatus status;
     @Size(max = 500, message = "Message must be at most 500 characters")
    private String message;
 
     public UpdateReminderDTO() {
     }
-    public UpdateReminderDTO(LocalDate reminderDate, String status, String message) {
+    public UpdateReminderDTO(LocalDate reminderDate, ReminderStatus status, String message) {
           this.reminderDate = reminderDate;
           this.status = status;
           this.message = message;
      }
 
-   public String getStatus() {
+   public ReminderStatus getStatus() {
     return status;
    }
 
-   public void setStatus(String status) {
+   public void setStatus(ReminderStatus status) {
     this.status = status;
    }
 
