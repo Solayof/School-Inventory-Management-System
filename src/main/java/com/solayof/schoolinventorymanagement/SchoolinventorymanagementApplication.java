a9c4bd3ea6ce1faf8c5a32c5ef28d93441cfdbd9
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-08-02 09:35:30 +0100

    add enableschudeling annotation

--- src/main/java/com/solayof/schoolinventorymanagement/SchoolinventorymanagementApplication.java
+++ src/main/java/com/solayof/schoolinventorymanagement/SchoolinventorymanagementApplication.java
@@ -2,8 +2,10 @@ package com.solayof.schoolinventorymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // Enables Spring's scheduled task execution
 public class SchoolinventorymanagementApplication {
 
 	public static void main(String[] args) {
