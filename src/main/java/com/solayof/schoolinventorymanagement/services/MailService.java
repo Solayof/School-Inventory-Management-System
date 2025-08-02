c6b187c2a59922b5a9c0f9882821df5ffd4dd084
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-08-02 09:42:50 +0100

    create mailservice

---
+++ src/main/java/com/solayof/schoolinventorymanagement/services/MailService.java
@@ -0,0 +1,24 @@
+package com.solayof.schoolinventorymanagement.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    
+    
+    public void sendEmail(String to, String subject, String body) throws MailException {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@schoolinventory.com"); // Configure this in application.properties
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        javaMailSender.send(message);
+    }
+}
