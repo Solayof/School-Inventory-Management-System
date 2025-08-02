13998bd069a67536bd3d0596e9fa97f928d02689
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-08-02 09:41:21 +0100

    add a method to assignment repository

--- src/main/java/com/solayof/schoolinventorymanagement/repository/AssignmentRepository.java
+++ src/main/java/com/solayof/schoolinventorymanagement/repository/AssignmentRepository.java
@@ -1,9 +1,12 @@
 package com.solayof.schoolinventorymanagement.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.solayof.schoolinventorymanagement.entity.Assignment;
 
 
 public interface AssignmentRepository extends JpaRepository<Assignment, UUID> {
+    List<Assignment> findByReturnDueDateBeforeAndActualReturnDateIsNull(LocalDate dueDate);
 }
