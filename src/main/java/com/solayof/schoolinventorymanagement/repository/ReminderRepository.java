350ad33990dc9ce47ee5064a79195b9dfac1a636
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2025-08-19 02:27:11 +0100

    update reminder repository

--- src/main/java/com/solayof/schoolinventorymanagement/repository/ReminderRepository.java
+++ src/main/java/com/solayof/schoolinventorymanagement/repository/ReminderRepository.java
@@ -1,13 +1,27 @@
 package com.solayof.schoolinventorymanagement.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.solayof.schoolinventorymanagement.constants.ReminderStatus;
 import com.solayof.schoolinventorymanagement.entity.Reminder;
 
 public interface ReminderRepository extends JpaRepository<Reminder, UUID> {
     List<Reminder> findByStatus(ReminderStatus status);
+
+    @Query("""
+        SELECT r FROM Reminder r
+        WHERE r.reminderDate BETWEEN :startTime AND :endTime
+        AND r.status IN (:statuses)
+    """)
+    List<Reminder> findRemindersDueWithinLastHour(
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime,
+            @Param("statuses") List<ReminderStatus> statuses
+    );
 }
